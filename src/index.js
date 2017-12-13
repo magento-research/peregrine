56bd3dae54288c7a8e2a27fe333ba10a0f05c5ad
jimbo <jimbo@users.noreply.github.com>
2017-12-13 11:45:14 -0700

    Refactor Peregrine to be a plain class
    Rather than being a React Component class, Peregrine needs to be
    a plain class that provides helpful methods to an app instance.
    
    This refactor uses async methods, which work well with dynamic
    imports.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
-import { Component, Fragment, createElement } from 'react';
+import { createElement } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import createStore from './store';
 
-class Peregrine extends Component {
-    constructor(props) {
-        super(props);
+const renderAsync = (element, container) =>
+    new Promise(resolve => render(element, container, resolve));
 
+class Peregrine {
+    constructor(component) {
+        this.component = component;
         this.store = createStore();
     }
 
-    static mount() {
-        render(...arguments);
-    }
-
-    render() {
-        const { props, store } = this;
+    async render() {
+        const { component, store } = this;
+        const Component = await component;
 
         return (
             <Provider store={store}>
                 <BrowserRouter>
-                    <Fragment>{props.children}</Fragment>
+                    <Component />
                 </BrowserRouter>
             </Provider>
         );
+    }
+
+    async mount(container) {
+        this.container = container;
+        this.element = await this.render();
+
+        await renderAsync(this.element, container);
+    }
+
+    async addReducer(key, _reducer) {
+        const reducer = await _reducer;
+
+        this.store.dispatch({
+            type: 'ADD_REDUCER',
+            payload: { key, reducer }
+        });
     }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -1,27 +1,33 @@
 import { createElement } from 'react';
 import { connect } from 'react-redux';
-import { configure, shallow } from 'enzyme';
+import { configure, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Peregrine from './index.js';
 
 configure({ adapter: new Adapter() });
 
-test('renders a single child transparently', () => {
-    const child = <i />;
-    const unit = <Peregrine>{child}</Peregrine>;
+const SimpleComponent = () => <i />;
 
-    expect(shallow(unit).html()).toEqual(shallow(child).html());
+test('constructs a new Peregrine instance', () => {
+    const received = new Peregrine(Promise.resolve(SimpleComponent));
+
+    expect(received).toMatchObject(
+        expect.objectContaining({
+            component: expect.any(Promise),
+            store: expect.objectContaining({
+                dispatch: expect.any(Function),
+                getState: expect.any(Function)
+            })
+        })
+    );
 });
 
-test('renders multiple children transparently', () => {
-    const children = [<i key="a" />, <i key="b" />];
-    const unit = <Peregrine>{children}</Peregrine>;
-    const expected = <div>{children}</div>;
+test('renders a Peregrine instance', async () => {
+    const received = await new Peregrine(
+        Promise.resolve(SimpleComponent)
+    ).render();
+    const expected = <SimpleComponent />;
 
-    expect(shallow(unit).html()).toEqual(
-        shallow(expected)
-            .children()
-            .reduce((value, node) => value + node.html(), '')
-    );
+    expect(render(received)).toEqual(render(expected));
 });
