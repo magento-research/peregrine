5fb2836be360272ec9b5f624ea3bae18375c8042
jimbo <jimbo@users.noreply.github.com>
2017-12-20 11:25:25 -0700

    Write tests for Peregrine methods

--- src/index.test.js
+++ src/index.test.js
@@ -24,10 +24,36 @@ test('constructs a new Peregrine instance', () => {
 });
 
 test('renders a Peregrine instance', async () => {
-    const received = await new Peregrine(
-        Promise.resolve(SimpleComponent)
-    ).render();
+    const app = new Peregrine(Promise.resolve(SimpleComponent));
+    const received = await app.render();
     const expected = <SimpleComponent />;
 
     expect(render(received)).toEqual(render(expected));
+});
+
+test('mounts a Peregrine instance', async () => {
+    const container = document.createElement('div');
+    const received = new Peregrine(Promise.resolve(SimpleComponent));
+    const expected = <SimpleComponent />;
+
+    await received.mount(container);
+
+    expect(received.container).toEqual(container);
+    expect(render(received.element)).toEqual(render(expected));
+    expect(container.firstChild.outerHTML).toEqual(render(expected).toString());
+});
+
+test('adds a reducer to the store', async () => {
+    const expected = {};
+    const app = new Peregrine(Promise.resolve(SimpleComponent));
+    const fooReducer = (state = null, { type }) =>
+        type === 'bar' ? expected : state;
+
+    await app.addReducer('foo', fooReducer);
+    const received = app.store.getState().foo;
+    expect(received).toBe(null);
+
+    app.store.dispatch({ type: 'bar' });
+    const next = app.store.getState().foo;
+    expect(next).toBe(expected);
 });
