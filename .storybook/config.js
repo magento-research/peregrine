08a25788b07312bcbc26f7e35310c764db2a4f98
Andrew Levine <theandrewtizer@gmail.com>
2018-04-19 12:16:37 -0500

    Deploy Storybook in PRs

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
     const context = require.context('../src', true, /__stories__\/.+\.js$/);
-    context.keys().map(context);
+    context.keys().forEach(context);
 }
 
 configure(loadStories, module);
